181f61353f06a9c7cca51f09038b73be638bc369
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2022-02-22 03:31:19 +0000

    pretty file

--- tests/testmbtiles.test.ts
+++ tests/testmbtiles.test.ts
@@ -1,21 +1,19 @@
-
 import { Test } from "tape";
 const test = require("tape");
 import { testURLAgainstSnap, tileURL, snapPath } from "./testutils";
 
-test("tile json for mbtiles can be served", function(t: Test) {
-  
-  testURLAgainstSnap(t,
+test("tile json for mbtiles can be served", function (t: Test) {
+  testURLAgainstSnap(
+    t,
     tileURL("bushfire_risk_localities.json"),
-    snapPath("mbtiles_json_res.json"));
-  
+    snapPath("mbtiles_json_res.json")
+  );
 });
 
-test("a tile for directories can be served", function(t: Test) {
-
-  testURLAgainstSnap(t,
+test("a tile for directories can be served", function (t: Test) {
+  testURLAgainstSnap(
+    t,
     tileURL("bushfire_risk_localities/5/29/18.vector.pbf"),
     snapPath("mbtiles_tile_res.pbf")
-  )
-
+  );
 });
