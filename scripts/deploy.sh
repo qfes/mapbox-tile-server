8f94119f170066a2843393193a0c5ca9cd1c18d5
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2022-02-23 07:20:33 +0000

    tee up deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sam deploy --guided
+sam deploy --guided --stack-name "mapbox-tile-server" --parameter-overrides 'endpoints="a.tiles.qfesanalytics.com,tiles.qfesanalytics.com",domainname="tiles.qfesanalytics.com",bucket="qfes-mapbox-tiles"'
